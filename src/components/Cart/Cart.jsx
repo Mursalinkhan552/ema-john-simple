1a5e8cc5a60fded97452a487ba3efbf5ceedc697
unknown <mursalinkhan552@gmail.com>
2023-11-29 12:28:32 +0600

    Added Local Storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,9 +8,17 @@ const Cart = (props) => {
     console.log(cart); 
     let totalPrice = 0;
     let totalShipping =0;
+    let quantity = 0;
     for(const product of cart){
-        totalPrice = totalPrice + product.price;
+        // product.quantity = product.quantity || 1;
+        if(product.quantity === 0){
+            product.quantity = 1;
+        }
+
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice * 7 / 100;
@@ -19,11 +27,11 @@ const Cart = (props) => {
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Select Item: {cart.length}</p>
+            <p>Select Item: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
-            <p>Total Shipping:{totalShipping}</p>
-            <p>Tax:{tax.toFixed(2)}</p>
-            <h6>Grand Total: {grandTotal.toFixed(2)}</h6>
+            <p>Total Shipping: {totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
